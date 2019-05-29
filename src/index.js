90e5b1f6694c1784f7073b20878958200b4623c9
Nicola Heald <nicola@notnowlewis.com>
2019-05-29 16:03:30 +0100

    Initial commit of search as you type widget

---
+++ src/index.js
@@ -0,0 +1,64 @@
+/** @jsx h */
+
+import { h, render, Component } from 'preact';
+
+class SearchResult extends Component {
+	render() {
+		const { result } = this.props;
+		return <p>{ result.title }</p>;
+	}
+}
+
+class SearchWidget extends Component {
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			query: this.props.initialValue,
+			results: []
+		};
+		this.onChangeQuery = this.onChangeQuery.bind( this );
+		this.getResults = this.getResults.bind( this );
+		this.getResults( this.props.initialValue );
+	}
+	getResults( query ) {
+		if ( query ) {
+			const results = [];
+			const numResults = Math.floor( Math.random() * (5 - 0 + 1) + 0 );
+			for ( let i = 0; i < numResults; i++ ) {
+				results.push( { title: 'Search result for ' + query } );
+			}
+			this.setState( { results } );
+		} else { 
+			this.setState( { results: [] } );
+		}
+	}
+	onChangeQuery( event ) {
+		const query = event.target.value;
+		this.setState( { query } );
+		this.getResults( query );
+	}
+	componentDidMount() {
+		this.input.focus();
+	}
+	render() {
+		const { query, results } = this.state;
+		// TODO: Results might want to be rendered into a separate element rather than inline.
+		return (
+			<div>
+				<p>You are searching for "{ query }".</p>
+				<p><input type="text" value={ query } onInput={ this.onChangeQuery } ref={ input => this.input = input } /></p>
+				{ results.map( ( result ) => <SearchResult result={ result } /> ) }
+			</div>
+		);
+	}
+}
+
+const injectSearchWidget = ( initialValue, target ) => {
+	render(
+		<SearchWidget initialValue={ initialValue } />,
+		document.body,
+		target
+	);
+};
+
+window.injectSearchWidget = injectSearchWidget;
